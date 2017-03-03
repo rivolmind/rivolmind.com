ab446febf6bdd1105fabb15eee97cdea9a7efc80
rivolmind <rivoltest@gmail.com>
2017-03-03 17:17:38 +0200

    chor: Added temp files from server

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<table>
+<!--<tr>
+	<td><div style="margin-left: 40px"><strong>RivolMind.Com - персональный семейный сайт –емизовых Ёли и ¬ани</strong></div></td>
+</tr>-->
+<tr>
+	<td> <img src="working.jpg" alt="test"></img> </td>
+</tr>
+</table>
+</<body>
+</html>
